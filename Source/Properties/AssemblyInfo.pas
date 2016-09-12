cb164b7d4b02766039b6ccde0eafda13fdc3a00a
marc hoffman <mh@remobjects.com>
2016-09-12 15:53:49 -0400

    AssenmblyInfo

---
+++ Source/Properties/AssemblyInfo.pas
@@ -0,0 +1,28 @@
+﻿namespace Elements.RTL.Delphi;
+
+interface
+
+uses
+  System.Reflection,
+  System.Resources,
+  System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle('')]
+[assembly: AssemblyDescription('')]
+[assembly: AssemblyConfiguration('')]
+[assembly: AssemblyCompany('')]
+[assembly: AssemblyProduct('')]
+[assembly: AssemblyCopyright('')]
+[assembly: AssemblyTrademark('')]
+[assembly: AssemblyCulture('')]
+[assembly: AssemblyVersion('1.0.0.1')]
+[assembly: NeutralResourcesLanguage('')]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile('')]
+[assembly: AssemblyKeyName('')]
+
+implementation
+
+end.
