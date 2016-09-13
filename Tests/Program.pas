4e8e6a480dcd4a7a3a3e00489b96e94bdf54aadd
marc hoffman <mh@remobjects.com>
2016-09-13 11:33:05 -0400

    Test project skeleton & first test

---
+++ Tests/Program.pas
@@ -0,0 +1,13 @@
+﻿namespace DelphiRTL.Tests;
+
+interface
+
+uses
+  RemObjects.Elements.EUnit;
+
+implementation
+
+begin
+  var lTests := Discovery.DiscoverTests();
+  Runner.RunTests(lTests) withListener(Runner.DefaultListener);
+end.
---
+++ Tests/String.Manipulations.pas
@@ -0,0 +1,24 @@
+﻿namespace DelphiRTL.Tests.Shared;
+
+uses
+  RemObjects.Elements.EUnit,
+  Sugar,
+  Elements.RTL.Delphi;
+
+type
+  StringManipulations = public class(Test)
+  private
+  protected
+  public
+    
+    method IndexerTests();
+    begin
+      var x: DelphiString := 'house';
+      x[1] := 'm';
+      Assert.AreEqual(x, 'mouse');
+
+      //Assert.AreEqual(x, 'mouse2');
+    end;
+  end;
+
+end.
