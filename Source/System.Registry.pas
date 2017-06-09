8b859c59778612726482a9c9af53c4846c0a3043
Diego Navarro <dinavarro@gmail.com>
2017-06-09 14:48:01 +0200

    Registry functions implemented
    Next step: tests and some refactor

--- Source/System.Registry.pas
+++ Source/System.Registry.pas
@@ -33,8 +33,9 @@ type
     fCurrentPath: DelphiString;
 
     method IsRelative(Key: DelphiString): Boolean;
-    method GetLastErrorMsg: DelphiString;
     method SetRootKey(Value: rtl.HKEY);
+    method ConvertDataType(Value: rtl.DWORD): TRegDataType;
+    method ConvertToDataType(Value: TRegDataType): rtl.DWORD;
   protected
     method ChangeKey(Value: rtl.HKey; Path: DelphiString);
     method CheckResult(RetVal: Longint): Boolean;
@@ -42,8 +43,8 @@ type
     method GetData(Name: DelphiString; Buffer: Pointer; BufSize: rtl.DWORD; var RegData: TRegDataType): Integer;
     method GetKey(Key: DelphiString): rtl.HKEY;
     method GetRootKeyName: DelphiString;
-    /*method PutData(const Name: string; Buffer: Pointer; BufSize: Integer; RegData: TRegDataType);
-    method SetCurrentKey(Value: HKEY);*/
+    method PutData(Name: DelphiString; Buffer: Pointer; BufSize: Integer; RegData: TRegDataType);
+    //method SetCurrentKey(Value: HKEY);*/
   public
     constructor;
     constructor(aAccess: LongWord);
@@ -53,50 +54,45 @@ type
     method CreateKey(Key: DelphiString): Boolean;
     method DeleteKey(Key: DelphiString): Boolean;
     method DeleteValue(Name: DelphiString): Boolean;
-    //method GetDataAsString(const ValueName: string; PrefixType: Boolean = false): string;
     method GetDataInfo(ValueName: DelphiString; var Value: TRegDataInfo): Boolean;
     method GetDataSize(ValueName: DelphiString): Integer;
     method GetDataType(ValueName: DelphiString): TRegDataType;
-
     method GetKeyInfo(var Value: TRegKeyInfo): Boolean;
     method GetKeyNames(Strings: TStrings);
     method GetValueNames(Strings: TStrings);
     method HasSubKeys: Boolean;
     method KeyExists(Key: DelphiString): Boolean;
     method LoadKey(Key, FileName: DelphiString): Boolean;
-    method MoveKey(OldName, NewName: DelphiString; Delete: Boolean);    
     method OpenKey(Key: DelphiString; CanCreate: Boolean): Boolean;
     method OpenKeyReadOnly(Key: DelphiString): Boolean;
-    /*method ReadCurrency(const Name: string): Currency;
-    method ReadBinaryData(const Name: string; var Buffer; BufSize: Integer): Integer;
-    method ReadBool(const Name: string): Boolean;
-    method ReadDate(const Name: string): TDateTime;
-    method ReadDateTime(const Name: string): TDateTime;
-    method ReadFloat(const Name: string): Double;
-    method ReadInteger(const Name: string): Integer;
-    method ReadString(const Name: string): string;
-    method ReadTime(const Name: string): TDateTime;*/
+
+    method ReadBinaryData(Name: DelphiString; var Buffer: Pointer; BufSize: Integer): Integer;
+    method ReadBool(Name: DelphiString): Boolean;
+    method ReadDate(Name: DelphiString): TDateTime;
+    method ReadDateTime(Name: DelphiString): TDateTime;
+    method ReadFloat(Name: DelphiString): Double;
+    method ReadInteger(Name: DelphiString): Integer;
+    method ReadString(Name: DelphiString): DelphiString;
+    method ReadTime(Name: DelphiString): TDateTime;
     method RegistryConnect(UNCName: DelphiString): Boolean;
-    method RenameValue(OldName, NewName: DelphiString);
     method ReplaceKey(Key, FileName, BackUpFileName: DelphiString): Boolean;
     method RestoreKey(Key, FileName: DelphiString): Boolean;
     method SaveKey(Key, FileName: DelphiString): Boolean;
     method UnLoadKey(Key: DelphiString): Boolean;
     method ValueExists(Name: DelphiString): Boolean;
-    /*method WriteCurrency(const Name: string; Value: Currency);
-    method WriteBinaryData(const Name: string; var Buffer; BufSize: Integer);
-    method WriteBool(const Name: string; Value: Boolean);
-    method WriteDate(const Name: string; Value: TDateTime);
-    method WriteDateTime(const Name: string; Value: TDateTime);
-    method WriteFloat(const Name: string; Value: Double);
-    method WriteInteger(const Name: string; Value: Integer);
-    method WriteString(const Name, Value: string);
-    method WriteExpandString(const Name, Value: string);
-    method WriteTime(const Name: string; Value: TDateTime);*/
+    
+    method WriteBinaryData(Name: DelphiString; Buffer: Pointer; BufSize: Integer);
+    method WriteBool(Name: DelphiString; Value: Boolean);
+    method WriteDate(Name: DelphiString; Value: TDateTime);
+    method WriteDateTime(Name: DelphiString; Value: TDateTime);
+    method WriteFloat(Name: DelphiString; Value: Double);
+    method WriteInteger(Name: DelphiString; Value: Integer);
+    method WriteString(Name, Value: DelphiString);
+    method WriteExpandString(Name, Value: DelphiString);
+    method WriteTime(Name: DelphiString; Value: TDateTime);
     property CurrentKey: rtl.HKEY read fCurrentKey;
     property CurrentPath: DelphiString read fCurrentPath;
     property LastError: Longint read fLastError;
-    property LastErrorMsg: DelphiString read GetLastErrorMsg;
     property RootKey: rtl.HKEY read FRootKey write SetRootKey;
     property RootKeyName: DelphiString read GetRootKeyName;
     property Access: LongWord read fAccess write fAccess;
@@ -150,8 +146,10 @@ end;
 
 method TRegistry.GetData(Name: DelphiString; Buffer: Pointer; BufSize: rtl.DWORD; var RegData: TRegDataType): Integer;
 begin
-  if not CheckResult(rtl.RegGetValue(CurrentKey, nil, Name.ToString.FirstChar, rtl.RRF_RT_ANY, @RegData, Buffer, @BufSize)) then
+  var lType: rtl.DWORD;
+  if not CheckResult(rtl.RegGetValue(CurrentKey, nil, Name.ToString.FirstChar, rtl.RRF_RT_ANY, @lType, Buffer, @BufSize)) then
     raise new Exception('Can not retrieve value for ' + Name);
+  RegData := ConvertDataType(lType);
   result := BufSize;
 end;
 
@@ -183,14 +181,38 @@ begin
   end;
 end;
 
-method TRegistry.GetLastErrorMsg: DelphiString;
+method TRegistry.PutData(Name: DelphiString; Buffer: Pointer; BufSize: Integer; RegData: TRegDataType);
 begin
+  if not CheckResult(rtl.RegSetKeyValue(CurrentKey, nil, Name.ToString.FirstChar, ConvertToDataType(RegData), Buffer, BufSize)) then
+    raise new Exception('Can not write ' + Name + ' value to registry');
 end;
 
 method TRegistry.SetRootKey(Value: rtl.HKEY);
 begin
   fRootKey := Value;
   CloseKey;
+end;
+
+method TRegistry.ConvertDataType(Value: rtl.DWORD): TRegDataType;
+begin
+  result := case Value of
+    rtl.REG_BINARY: TRegDataType.Binary;
+    rtl.REG_DWORD: TRegDataType.Integer;
+    rtl.REG_SZ: TRegDataType.String;
+    rtl.REG_EXPAND_SZ: TRegDataType.ExpandString;
+    else TRegDataType.Unknown;
+  end;
+end;
+
+method TRegistry.ConvertToDataType(Value: TRegDataType): rtl.DWORD;
+begin
+  result := case Value of
+    TRegDataType.Binary: rtl.REG_BINARY;
+    TRegDataType.Integer: rtl.REG_DWORD;
+    TRegDataType.String: rtl.REG_SZ;
+    TRegDataType.ExpandString: rtl.REG_EXPAND_SZ;
+    else rtl.REG_NONE;
+  end;
 end;
 
 method TRegistry.CloseKey;
@@ -239,7 +261,9 @@ end;
 
 method TRegistry.GetDataInfo(ValueName: DelphiString; var Value: TRegDataInfo): Boolean;
 begin
-  result := CheckResult(rtl.RegGetValue(CurrentKey, nil, ValueName.ToString.FirstChar, rtl.RRF_RT_ANY, @Value.RegData, nil, @Value.DataSize));
+  var lType: rtl.DWORD;
+  result := CheckResult(rtl.RegGetValue(CurrentKey, nil, ValueName.ToString.FirstChar, rtl.RRF_RT_ANY, @lType, nil, @Value.DataSize));
+  Value.RegData := ConvertDataType(lType);
 end;
 
 method TRegistry.GetDataSize(ValueName: DelphiString): Integer;
@@ -332,11 +356,6 @@ begin
   result := CheckResult(rtl.RegLoadKey(fRootKey, Key.ToString.FirstChar, FileName.ToString.FirstChar));
 end;
 
-method TRegistry.MoveKey(OldName, NewName: DelphiString; Delete: Boolean);    
-begin
-
-end;
-
 method TRegistry.OpenKey(const Key: DelphiString; CanCreate: Boolean): Boolean;
 begin
   CloseKey;
@@ -383,6 +402,65 @@ begin
   end;
 end;
 
+method TRegistry.ReadBinaryData(Name: DelphiString; var Buffer: Pointer; BufSize: Integer): Integer;
+begin
+  var lRegData: TRegDataType;
+  result := GetData(Name, Buffer, BufSize, var lRegData);
+  if lRegData <> TRegDataType.Binary then
+    raise new Exception('Can not read ' + Name + ' as binary data');
+end;
+
+method TRegistry.ReadBool(Name: DelphiString): Boolean;
+begin
+  result := ReadInteger(Name) > 0;
+end;
+
+method TRegistry.ReadDate(Name: DelphiString): TDateTime;
+begin
+  result := ReadDateTime(Name);
+end;
+
+method TRegistry.ReadDateTime(Name: DelphiString): TDateTime;
+begin
+  var lRegData: TRegDataType;
+  GetData(Name, @result, sizeOf(result), var lRegData);
+  if lRegData <> TRegDataType.Binary then
+    raise new Exception('Can not read ' + Name + ' as datetime value');
+end;
+
+method TRegistry.ReadFloat(Name: DelphiString): Double;
+begin
+  var lRegData: TRegDataType;
+  GetData(Name, @result, sizeOf(result), var lRegData);
+  if lRegData <> TRegDataType.Binary then
+    raise new Exception('Can not read ' + Name + ' as float value');
+end;
+
+method TRegistry.ReadInteger(Name: DelphiString): Integer;
+begin
+  var lRegData: TRegDataType;
+  GetData(Name, @result, sizeOf(result), var lRegData);
+  if lRegData <> TRegDataType.Integer then
+    raise new Exception('Can not read ' + Name + ' as integer value');
+end;
+
+method TRegistry.ReadString(Name: DelphiString): DelphiString;
+begin
+  var lSize := GetDataSize(Name);
+  var lChars := new Char[lSize div sizeOf(Char)];
+  var lRegData: TRegDataType;
+  var lTotal := GetData(Name, @lChars[0], lSize, var lRegData);
+  if (lRegData = TRegDataType.String) or (lRegData = TRegDataType.ExpandString) then
+    result := DelphiString.Create(lChars, 0, lTotal div sizeOf(Char))
+  else
+    raise new Exception('Can no read ' + Name + ' as string value');
+end;
+
+method TRegistry.ReadTime(Name: DelphiString): TDateTime;
+begin
+  result := ReadDateTime(Name);
+end;
+
 method TRegistry.RegistryConnect(UNCName: DelphiString): Boolean;
 begin
   var lNewKey: rtl.HKEY;
@@ -391,14 +469,14 @@ begin
     fRootKey := lNewKey;
 end;
 
-method TRegistry.RenameValue(OldName, NewName: DelphiString);
-begin
-
-end;
-
 method TRegistry.ReplaceKey(Key, FileName, BackUpFileName: DelphiString): Boolean;
 begin
+  var lRelative := IsRelative(Key);
+  var lKey := Key;
+  if lRelative then
+    lKey := Key.SubString(1);
 
+  result := CheckResult(rtl.RegReplaceKey(GetBaseKey(lRelative), lKey.ToString.FirstChar, FileName.ToString.FirstChar, FileName.ToString.FirstChar));
 end;
 
 method TRegistry.RestoreKey(Key, FileName: DelphiString): Boolean;
@@ -424,6 +502,52 @@ method TRegistry.ValueExists(Name: DelphiString): Boolean;
 begin
   var lType: rtl.DWORD;
   result := CheckResult(rtl.RegQueryValueEx(CurrentKey, Name.ToString.FirstChar, nil, @lType, nil, nil));
+end;
+
+method TRegistry.WriteBinaryData(Name: DelphiString; Buffer: Pointer; BufSize: Integer);
+begin
+  PutData(Name, Buffer, BufSize, TRegDataType.Binary);
+end;
+
+method TRegistry.WriteBool(Name: DelphiString; Value: Boolean);
+begin
+  WriteInteger(Name, Integer(Value));
+end;
+
+method TRegistry.WriteDate(Name: DelphiString; Value: TDateTime);
+begin
+  WriteDateTime(Name, Value);
+end;
+
+method TRegistry.WriteDateTime(Name: DelphiString; Value: TDateTime);
+begin
+  PutData(Name, @Value, sizeOf(Double), TRegDataType.Binary);
+end;
+
+method TRegistry.WriteFloat(Name: DelphiString; Value: Double);
+begin
+  PutData(Name, @Value, sizeOf(Double), TRegDataType.Binary);
+end;
+
+method TRegistry.WriteInteger(Name: DelphiString; Value: Integer);
+begin
+  PutData(Name, @Value, sizeOf(Integer), TRegDataType.Integer);
+end;
+
+method TRegistry.WriteString(Name, Value: DelphiString);
+begin
+  var lChars := Value.ToString.ToCharArray(true);
+  PutData(Name, @lChars[0], lChars.Length * sizeOf(Char), TRegDataType.String);
+end;
+
+method TRegistry.WriteExpandString(Name, Value: DelphiString);
+begin
+  WriteString(Name, Value);
+end;
+
+method TRegistry.WriteTime(Name: DelphiString; Value: TDateTime);
+begin
+  WriteDateTime(Name, Value);
 end;
 
 {$ENDIF}
