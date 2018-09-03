8ec064cad13e82afe33d2cec3c6360008156fc22
Diego Navarro <diegon@remobjects.com>
2018-09-03 17:32:30 +0200

    WPF ListBox and ComboBox functionality

--- Source/VCL/StdCtrls.WPF.pas
+++ Source/VCL/StdCtrls.WPF.pas
@@ -211,22 +211,24 @@ end;
 
 method TListControlItems.PlatformAddItem(S: DelphiString; aObject: TObject);
 begin
-
+  (ListControl.Handle as ListBox).Items.Add(S);
 end;
 
 method TListControlItems.PlatformInsert(aIndex: Integer; S: DelphiString);
 begin
-
+  var lItem := new ListBoxItem();
+  lItem.Content := S;
+  (ListControl.Handle as ListBox).Items.Insert(aIndex, lItem);
 end;
 
 method TListControlItems.PlatformClear;
 begin
-
+  (ListControl.Handle as ListBox).Items.Clear;
 end;
 
 method TListControlItems.PlatformDelete(aIndex: Integer);
 begin
-
+  (ListControl.Handle as ListBox).Items.RemoveAt(aIndex);
 end;
 
 method TListBox.CreateHandle;
@@ -236,72 +238,82 @@ end;
 
 method TListBox.PlatformSelectAll;
 begin
-
+  (fHandle as ListBox).SelectAll;
 end;
 
 method TListBox.PlatformGetSelected(aIndex: Integer): Boolean;
 begin
-
+  var lItem := (fHandle as ListBox).Items[aIndex];
+  result := (fHandle as ListBox).SelectedItems.Contains(lItem);
 end;
 
 method TListBox.PlatformSetSelected(aIndex: Integer; value: Boolean);
 begin
-
+  var lItem := (fHandle as ListBox).Items[aIndex];
+  if value then
+    (fHandle as ListBox).SelectedItems.Add(lItem)
+  else
+    (fHandle as ListBox).SelectedItems.Remove(lItem);
 end;
 
 method TListBox.PlatformGetSelCount: Integer;
 begin
-
+  result := (fHandle as ListBox).SelectedItems.Count;
 end;
 
 method TListBox.PlatformGetMultiSelect: Boolean;
 begin
-
+  result := (fHandle as ListBox).SelectionMode ≠ SelectionMode.Single;
 end;
 
 method TListBox.PlatformSetMultiSelect(value: Boolean);
 begin
-
+  if value then
+    (fHandle as ListBox).SelectionMode := SelectionMode.Extended
+  else
+    (fHandle as ListBox).SelectionMode := SelectionMode.Single;
 end;
 
 method TListBox.PlatformClearSelection;
 begin
-
+  (fHandle as ListBox).SelectedItems.Clear;
 end;
 
 method TListBox.PlatformDeleteSelected;
 begin
-
+  (fHandle as ListBox).Items.Remove((fHandle as ListBox).SelectedItem);
 end;
 
 method TListBox.PlatformSetItemIndex(value: Integer);
 begin
-
+  (fHandle as ListBox).SelectedIndex := value;
 end;
 
 method TListBox.PlatformGetItemIndex: Integer;
 begin
-
+  result := (fHandle as ListBox).SelectedIndex;
 end;
 
 method TComboBoxItems.PlatformAddItem(S: DelphiString; aObject: TObject);
 begin
-
+  (ListControl.Handle as ComboBox).Items.Add(S);
 end;
 
 method TComboBoxItems.PlatformInsert(aIndex: Integer; S: DelphiString);
 begin
-
+  var lItem := new ComboBoxItem();
+  lItem.Content := S;
+  (ListControl.Handle as ComboBox).Items.Insert(aIndex, lItem);
 end;
 
 method TComboBoxItems.PlatformClear;
 begin
-
+  (ListControl.Handle as ComboBox).Items.Clear;
 end;
 
 method TComboBoxItems.PlatformDelete(aIndex: Integer);
 begin
-
+  (ListControl.Handle as ComboBox).Items.RemoveAt(aIndex);
 end;
 
 method TComboBox.CreateHandle;
@@ -311,47 +323,54 @@ end;
 
 method TComboBox.PlatformGetText: String;
 begin
-
+  result := (fHandle as ComboBox).Text;
 end;
 
 method TComboBox.PlatformSetText(aValue: String);
 begin
-
+  (fHandle as ComboBox).Text := aValue;
 end;
 
 method TComboBox.PlatformSetOnSelect(aValue: TNotifyEvent);
 begin
-
+  (fHandle as ComboBox).SelectionChanged += new System.Windows.Controls.SelectionChangedEventHandler((s, e)-> begin
+    if fOnSelect ≠ nil then
+      fOnSelect(self);
+  end);
 end;
 
 method TComboBox.PlatformSetOnChange(aValue: TNotifyEvent);
 begin
-
+  (fHandle as ComboBox).AddHandler(System.Windows.Controls.Primitives.TextBoxBase.TextChangedEvent, new System.Windows.RoutedEventHandler((s, e) -> begin
+    if fOnChange ≠ nil then
+      fOnChange(self);
+  end));
 end;
 
 method TComboBox.PlatformSelectAll;
 begin
-
+  // Nothing to do here...
 end;
 
 method TComboBox.PlatformClearSelection;
 begin
-
+  (fHandle as ComboBox).SelectedIndex := -1;
 end;
 
 method TComboBox.PlatformDeleteSelected;
 begin
-
+  if (fHandle as ComboBox).SelectedItem ≠ nil then
+    (fHandle as ComboBox).Items.Remove((fHandle as ComboBox).SelectedItem);
 end;
 
 method TComboBox.PlatformSetItemIndex(value: Integer);
 begin
-
+  (fHandle as ComboBox).SelectedIndex := value;
 end;
 
 method TComboBox.PlatformGetItemIndex: Integer;
 begin
-
+  result := (fHandle as ComboBox).SelectedIndex;
 end;
 
 {$ENDIF}
