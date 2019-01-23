512d55409f8c631c837d581b774542fa696e75ab
Diego Navarro <dinavarro@gmail.com>
2019-01-23 19:01:05 +0100

    Implementing basic TTreeNode WPF functions, preparing TreeView functionality for mac

--- Source/VCL/ComCtrls.TreeView.Mac.pas
+++ Source/VCL/ComCtrls.TreeView.Mac.pas
@@ -1,25 +1,52 @@
 ﻿namespace RemObjects.Elements.RTL.Delphi.VCL;
 
-{$IF MACOS AND NOT (ISLAND AND DARWIN)}
+// TODO FIX THAT BEFORE MERGING!!!!!!!!!!!!!!!!
+
+//{$IF MACOS AND NOT (ISLAND AND DARWIN)}
+{$IF MACOS}
 
 interface
 
+uses
+  RemObjects.Elements.RTL.Delphi,
+  AppKit;
+
 type
+  TTreeNode = public partial class(TPersistent)
+  private
+    fChilds: TList<TTreeNode>;
+  end;
+
+  TTreeNodes = public partial class(TPersistent)
+  private
+    fNodes: TList<TTreeNode>;
+  end;
+
+
   TTreeView = public partial class(TNativeControl)
   private
   assembly
-    //fController: TListViewController;
+    fController: TTreeViewController;
   protected
     method CreateHandle; override;
   public
   end;
 
+  TTreeViewController = class(NSOutlineViewDelegate)
+  public
+
+  end;
+
+  TTreeViewDataSource = class(NSOutlineViewDataSource)
+  end;
 
 implementation
 
 method TTreeView.CreateHandle;
 begin
-  fHandle := new AppKit.NSOutlineView();
+  fHandle := new NSOutlineView();
+  fController := new TTreeviewController();
+  (fHandle as NSOutlineView).delegate := fController;
 end;
 
 {$ENDIF}
