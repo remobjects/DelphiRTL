1e4e4d0ca2e2c68511d0e15212c6e2b204db10f7
marc hoffman <mh@remobjects.com>
2020-02-11 08:31:25 -0400

    SetLength(array)

---
+++ Source/Array.pas
@@ -0,0 +1,20 @@
+﻿namespace RemObjects.Elements.RTL.Delphi;
+
+interface
+
+method SetLenth<T>(var aArray: array of T; aNewLength: Integer);
+
+implementation
+
+method SetLenth<T>(var aArray: array of T; aNewLength: Integer);
+begin
+  var len := length(aArray);
+  if len = aNewLength then
+    exit;
+  var lResult := new T[aNewLength];
+  for i: Integer := 0 to Min(len, aNewLength)-1 do
+    lResult[i] := aArray[i];
+  aArray := lResult;
+end;
+
+end.
