0706c7a7755da97c48fb973cf06e473ac5df25e4
marc hoffman <mh@remobjects.com>
2020-02-12 08:20:01 -0400

    SetLength(array), fix for Java and Island

--- Source/Array.pas
+++ Source/Array.pas
@@ -8,13 +8,17 @@ implementation
 
 method SetLenth<T>(var aArray: array of T; aNewLength: Integer);
 begin
-  var len := length(aArray);
+  var len: Integer := length(aArray);
   if len = aNewLength then
     exit;
+  {$IF COOPER}
+  aArray := java.util.Arrays.copyOf(aArray, aNewLength);
+  {$ELSE}
   var lResult := new T[aNewLength];
   for i: Integer := 0 to Min(len, aNewLength)-1 do
     lResult[i] := aArray[i];
   aArray := lResult;
+  {$ENDIF}
 end;
 
 end.
