82c5259a40ce3b44cab923f3ea78dc3809a6f05e
evgenyk <git@evgenysoft.com>
2024-07-10 14:48:13 +0300

    assembly name was fixed

---
+++ Source/VCL/Properties/AssemblyInfo.pas
@@ -0,0 +1,26 @@
+﻿namespace Elements.RTL.Delphi;
+
+interface
+
+uses
+  System.Reflection,
+  System.Resources,
+  System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle('RemObjects Elements Delphi VCL')]
+[assembly: AssemblyDescription('')]
+[assembly: AssemblyConfiguration('')]
+[assembly: AssemblyCompany('RemObjects Software, LLC')]
+[assembly: AssemblyProduct('RemObjects Elements')]
+[assembly: AssemblyCopyright('')]
+[assembly: AssemblyTrademark('')]
+[assembly: AssemblyCulture('')]
+[assembly: AssemblyVersion('1.0.0.1')]
+[assembly: NeutralResourcesLanguage('')]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyKeyName('RemObjectsSoftware')]
+
+implementation
+
+end.
